356476e7af1010a1d0c60e49c220f647286a2c13
Aziyat Murzamidinov <15@kross.kr>
2024-08-13 01:51:29 +0900

    feat: configure prettier

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -35,7 +35,10 @@ const LoginPage: React.FC = () => {
           {error && <p className="text-red-500">{error}</p>}
           <form onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Email
               </label>
               <input
@@ -49,7 +52,10 @@ const LoginPage: React.FC = () => {
               />
             </div>
             <div>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
                 Password
               </label>
               <input
